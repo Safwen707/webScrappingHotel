c46ef4388d5f3408a70fa60d709930c12f885b7b
unknown <safweneessayess@gmail.com>
2024-07-18 21:06:08 +0100

    hotels

--- hotel.js
+++ hotel.js
@@ -4,33 +4,52 @@ const mystery="https://tn.tunisiebooking.com/?utm_source=google&utm_medium=cpc&u
 const hotel_data = []
 async function getHotels (url) {
 try{
-    const response= await axios.get(url);
-    const loadData =cheerio.load (response.data);
-    const hotels = loadData(".entete_produit");
+    const {data}= await axios.get(url);
+    const $ =cheerio.load (data);
+    const box_produit = $("div.box_produit");
     
-    const localisation =loadData (".adresse_hotel ").text().trim();
+    
+    
    
-    
-    lieu=""
-    hotels.each(function()
-    {
-        nom =loadData(this).find (".bloc_titre").text().trim();
-        prix =loadData(this).find (".prix_produit").text().trim()
-        date_prix=loadData(this).find (".date_prix").text().trim()
-        hotel_data.push({nom,prix,lieu, date_prix})
+    box_produit.each(function()
+    {   
+        const entet_produit =$(this).find("div.entete_produit");
+        entet_produit.each(function(){
+        nom =$(this).find (".bloc_titre").text().trim();
+        prix =$(this).find (".prix_produit").text().trim()
+        date_prix=$(this).find (".date_prix").text().trim()
+        imgEtoileSrc = $(this).find('img').attr('src');
+        lieu = $(this).find('span.adresse_hotel').text().trim()||""
+        
+        
+        })
+        img_hotel_in = $(this).find("div.img_hotel_in");
+        img_hotel_in.each(function(i,elem){
+            imgHoteleSrc=$(elem).find('img.image_hotel').attr('src');
+
+        })
+        hotel_data.push({nom,prix, date_prix,lieu,imgEtoileSrc,imgHoteleSrc})
+        console.log("🚀 ~ getHotels ~ hotel_data:", hotel_data)
+
+        // imgHotelSrc = $(this).find('img.image_hotel').attr('src');
+        
+        
+        
+       
        
         
     });
+    // console.log("🚀 ~ getHotels ~ hotel_data:", hotel_data)
    
-    const localisations=localisation.split(" ")
-    console.log("🚀 ~ getHotels ~  localisations:",  localisations)
-    i=0
-    hotel_data.map(hotel => {
-        hotel.lieu = localisations[i]
-        i+=1;
-    }
+    // const localisations=localisation.split(" ")
+    
+    // i=0
+    // hotel_data.map(hotel => {
+    //     hotel.lieu = localisations[i]
+    //     i+=1;
+    // }
          
-    )
+    // )
     // console.log("🚀 ~ getHotels ~ Hotels:", hotel_data)
     
     
