34cd07d9c313fc2c52f38d7f6f53f92f19db8789
safwen707 <safweneessayess@gmail.com>
2024-07-23 15:44:03 +0100

    datePicker

--- repository/getPrice.js
+++ repository/getPrice.js
@@ -3,42 +3,83 @@ const cheerio= require("cheerio")
 const Hotel = require('../models/Hotel')
 const urlH="https://tn.tunisiebooking.com/detail_hotel_36/"
 const puppeteer=require('puppeteer')
-
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+ }
 async function getPrice(url){
     
-   
-
-    const browser=await puppeteer.launch({
-        headless:false
+const browser=await puppeteer.launch({
+    headless:false,//tet7al fenetre 9odemk 
+    slowMo: 50
         
-    })
-    console.log('Browser launched.');
-    const page=await browser.newPage()
-    console.log('New page created.');
-    await page.goto(url)
-    console.log('Navigated to target URL.');
+})
+const {data}= await axios.get(url);
+const $ =cheerio.load (data);   
+console.log('Browser launched.'); 
+const page=await browser.newPage()
+console.log('New page created.');
+await page.goto(url)
+console.log('Navigated to target URL.');
     
 
 
-
-
-// await page.type("#depart","22/08/2024") //(deux params le champs ou vous veuillez ecrire et le éeme param et la valeur a ecrir)
-// 
-console.log("arrivee")
     
 await page.click("#arrivee.v37_73")
-await page.$eval('input#checkin',(e) => e.removeAttribute("readonly"));  
+console.log("arrivee")
+var table = await page.evaluate(()=>{
+    return( document.getElementsByTagName('tbody'));
+
+})
+console.log("🚀  table:",  table)
+Object.keys(table).forEach(function(key, index) {
+    table[key] =table[key].tr;
+    
+  });
+  console.log("🚀 ~ Object.keys ~  table:",  table)
+// for (var i = 0, row; row = table.rows[i]; i++) {
+//    //iterate through rows
+//    //rows would be accessed using the "row" variable assigned in the for loop
+//    for (var j = 0, col; col = row.cells[j]; j++) {
+//      //iterate through columns
+//      //columns would be accessed using the "col" variable assigned in the for loop
+//    }  
+// }
+
+
+
+
+
+
+
+
+// await page.waitForSelector('body > div.daterangepicker.ltr.auto-apply.single.opensleft.show-calendar > div.drp-calendar.left.single > div.calendar-table > table > tbody', { visible: true });
+await delay(1000)
+const datePicker =await page.$$eval("body > div:nth-child(10) > div.drp-calendar.left.single > div.calendar-table > table > tbody",el => el.textContent)
+
+console.log("🚀 ~ datePicker ~ datePicker:", datePicker) 
+
+await page.click('body > div.daterangepicker.ltr.auto-apply.single.opensleft.show-calendar > div.drp-calendar.left.single > div.calendar-table > table > tbody ')
+console.log('clicked') 
+await delay(10000)
+
+
+
+// await page.$eval('input#checkin',(e) => e.removeAttribute("readonly")); 
+// await page.$eval('input#checkin',(e) => {e.value="25/07/2024" });  
+// await page.click(".col_dep")
+// await page.$eval('input#checkout.checkout',(e) => e.removeAttribute("readonly"));  
+// await page.$eval('input#checkout.checkout',(e) => e.value="31/07/2024");  
  
 
-await page.$eval('input#checkout.checkout',(e) => e.removeAttribute("readonly"));  
-await page.$eval('input#checkout.checkout',(e) => e.value="28/07/2024");  
 
- 
 
- await page.click(".v162_15") 
- await Promise.all([await page.$eval('input#checkin',(e) => {e.value="26/07/2024" }),page.click(".v194_22"),page.waitForNavigation({waitUntil:'networkidle2'})])
+await Promise.all([page.click(".v194_22")])  
+  
 
- 
+
+
 
 
 
@@ -54,7 +95,7 @@ const  HotelsPromo= await page.evaluate(()=>{
         }
     }
     
-    
+     
     let old_prices=[]
     let old_prices1=Array.from(document.querySelectorAll(".prix_barre")).map(x=>{return{ old_prix:x.textContent.trim()}})
     for(p of old_prices1){
@@ -71,13 +112,14 @@ const  HotelsPromo= await page.evaluate(()=>{
     return HotelsPromo
     
 })
-
+ 
 
 console.log("🚀 ~ getPrice ~ HotelsPromo:", HotelsPromo)
-
-// browser.close() 
+await delay(5000)
+browser.close() 
    
-    
-}
+     
+} 
  
-module.exports = {getPrice};
+ 
+getPrice("https://tn.tunisiebooking.com/theme/rsltv2_new_v2.php?item=c291cmNlX2NvbW09d2ViK2Rlc2t0b3AmdmlsbGVfdHh0PUhhbW1hbWV0JmlkX3htbF9ob3RlbD0mdmlsbGU9SGFtbWFtZXQmZGVwPTI4JTJGMDclMkYyMDI0JmRlcGFydD0yOCUyRjA3JTJGMjAyNCZkZmluPTMxJTJGMDclMkYyMDI0JmFycml2ZWU9MzElMkYwNyUyRjIwMjQmbmJyX251aXQ9MyZyZXRvdXJoPTI0JTJGMDclMkYyMDI0JmNoYW1icmVzPTEmbGFzdGNoPTEmYWN0aXZlPTEmYWR1bHRlczE9MiZlbmZhbnRzMT0xJmFnZTFfMT0yJmFnZTFfMj0mYWdlMV8zPSZhZHVsdGVzMj0wJmVuZmFudHMyPTAmYWdlMl8xPSZhZ2UyXzI9JmFnZTJfMz0mYWR1bHRlczM9MCZlbmZhbnRzMz0wJmFnZTNfMT0mYWdlM18yPSZhZ2UzXzM9JmFkdWx0ZXM0PTAmZW5mYW50czQ9MCZhZ2U0XzE9JmFnZTRfMj0mYWdlNF8zPSZhZHVsdGVzNT0wJmVuZmFudHM1PTAmYWdlNV8xPSZhZ2U1XzI9JmFnZTVfMz0%3D")
