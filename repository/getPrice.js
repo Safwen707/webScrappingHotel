f1288c6acbea2a30954ea9a0bdbf27ae0a029b8d
safwen707 <safweneessayess@gmail.com>
2024-07-24 15:23:20 +0100

    get hotels from date : it work

--- repository/getPrice.js
+++ repository/getPrice.js
@@ -11,76 +11,97 @@ function delay(time) {
 async function getPrice(url){
     
 const browser=await puppeteer.launch({
-    headless:false,//tet7al fenetre 9odemk 
-    slowMo: 50
-        
-})
-const {data}= await axios.get(url);
-const $ =cheerio.load (data);   
-console.log('Browser launched.'); 
+    headless:false,//tet7al fenetre 9odemk     
+})  
+console.log('Browser launched.');
+
 const page=await browser.newPage()
 console.log('New page created.');
-await page.goto(url)
+
+ await page.goto(url)
 console.log('Navigated to target URL.');
     
 
 
-    
-await page.click("#arrivee.v37_73")
-console.log("arrivee")
+
+
+
 //-------------------------------selectionner le date----------------
-await page.waitForSelector('.calendar-table tbody');// to avoid the problem TimeoutError: Navigation timeout of 30000 ms exceeded
-var tableRows = await page.evaluate(()=>{
-    const tbody = document.querySelector('.calendar-table tbody');
-  const rows=Array.from( tbody.getElementsByTagName('tr'));
-  return rows.map(row => {
-    const cells = Array.from(row.getElementsByTagName('td'));
-    return cells.map(cell => {
-        
-        if(cell.textContent=="25"){
-            return({text: cell.textContent,dataTitle:cell.getAttribute('data-title')})
-        }
-        });
-  });
+// await page.waitForSelector('.left tbody');// to avoid the problem TimeoutError: Navigation timeout of 30000 ms exceeded
+// const getDateTable = async function(jour){
+//     let tableRows = await page.evaluate(()=>{
+//         const tbody = document.querySelector('.left tbody');
+//          const rows=Array.from( tbody.getElementsByTagName('tr'));
+//          return rows.map((row) => {
+//            const cells = Array.from(row.getElementsByTagName('td'));
+//            return cells.map((cell) => {
+               
+//                if(cell.textContent=="25"){
+//                    return({text: cell.textContent,dataTitle:cell.getAttribute('data-title')})
+//                }
+//                });
+//          });
+       
+//        })
+//        return tableRows
+// }
 
-})
-
-
-const getDataTitle=function(t){
-    for (let i=0;i<t.length;i++){
-        for (let j=0;j<t[i].length;j++){
-            if (t[i][j]!=null){
-                const data_title=t[i][j]
-                return data_title
-            }
-        }
+// const getDataTitle=function(t){
+//     for (let i=0;i<t.length;i++){
+//         for (let j=0;j<t[i].length;j++){
+//             if (t[i][j]!=null){
+//                 const data_title=t[i][j]
+//                 return data_title
+//             }
+//         }
     
-    }
-}
-let data_tiltle=getDataTitle(tableRows)
-const dataTitleValue =data_tiltle.dataTitle
-console.log("🚀 ~ getPrice ~ data_tiltle:", data_tiltle)
-console.log("🚀 ~ getPrice ~ dataTitleValue:", dataTitleValue)
+//     }
+// }
 
-await page.waitForSelector(`[data-title=${dataTitleValue}]`)
-await page.click(`[data-title=${dataTitleValue}]`);
+// let dateTable=await getDateTable()
 
-console.log('clicked') 
+// let data_tiltle=getDataTitle(dateTable)
+// const dataTitleValue =data_tiltle.dataTitle
 
+// console.log("🚀 ~ getPrice ~ dataTitleValue:", (dataTitleValue))
+// const navigationPromise = page.waitForNavigation();
+// await page.waitForSelector(`[data-title=${dataTitleValue}]`)
+// await page.click(`[data-title=${dataTitleValue}]`);
+// await Promise.all[navigationPromise];
 
+// await delay(1000)
 
-//------------------------------ecrire le date---------------------------------
-// await page.$eval('input#checkin',(e) => e.removeAttribute("readonly")); 
-// await page.$eval('input#checkin',(e) => {e.value="25/07/2024" });  
-// await page.click(".col_dep")
-// await page.$eval('input#checkout.checkout',(e) => e.removeAttribute("readonly"));  
-// await page.$eval('input#checkout.checkout',(e) => e.value="31/07/2024");  
- 
+// await page.click('th.next.available')
+// await page.click('th.next.available')
 
 
 
- await Promise.all([page.click(".v194_22")])  
-  
+
+
+//------------------------------arrivé---------------------------------
+await page.waitForSelector("#arrivee.v37_73")     
+await page.click("#arrivee.v37_73")
+await page.waitForSelector("#depart") //ma9loubin fe site
+await page.$eval('#depart',(e) => {e.value="26/07/2024" });
+await page.click("#arrivee.v37_73")
+
+//------------------------------depart---------------------------------
+
+await page.waitForSelector(".col_dep") 
+await page.click(".col_dep")
+await page.waitForSelector("input#checkout.checkout") 
+await page.$eval('input#checkout.checkout',(e) => e.value="29/07/2024"); 
+
+//------------------------------search---------------------------------
+await page.waitForSelector(".v194_22") 
+await page.click(".v194_22")
+await delay(2000)
+// -----------------que les hotels disponible--------
+await page.waitForSelector("#btn_filtre_hotel_dispo_bleu") 
+await page.click("#btn_filtre_hotel_dispo_bleu")
+await delay(2000) 
+
+
 
 
 
@@ -120,7 +141,7 @@ const  HotelsPromo= await page.evaluate(()=>{
 
 console.log("🚀 ~ getPrice ~ HotelsPromo:", HotelsPromo)
 
-browser.close() 
+// browser.close() 
    
      
 } 
