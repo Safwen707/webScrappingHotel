b3e2ac1fc063d7aaab6aee5eba45595adc8247e9
safwen707 <safweneessayess@gmail.com>
2024-07-30 12:30:20 +0100

    ma sa7lich

--- index.js
+++ index.js
@@ -1,152 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+repository1=require('./repository/hotels')
+repository2=require('./repository/getPrice')
+const linkDatabase = require('./config/db')
 
-const puppeteer=require('puppeteer')
-function delay(time) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve, time)
-    });
- }
-
-async function getPrice(url,destination,date_depart,date_arrivee,nbrAdulte,nbrEnfant){
-// let date_arrivee=new Date().toJSON().slice(0,10).split('-').reverse().join('/') 
-const browser=await puppeteer.launch({
-    headless:false,//tet7al fenetre 9odemk     
-})  
-console.log('Browser launched.');
-
-const page=await browser.newPage()
-console.log('New page created.');
-
- await page.goto(url)
-console.log('Navigated to target URL.');
-    
+app.use(express.json());
 
 
+const port=process.env.PORT ||3000
+app.listen(port,()=>{
+    console.log(`running on port ${port}`)
+});
 
-//------------------------------destination---------------------------------
-await page.waitForSelector("#search")     
-await page.click("#search")
-await page.waitForSelector("#ville_des")     
-await page.$eval('#ville_des',(e,destination) => {e.value=destination },destination);
+linkDatabase();
 
-//------------------------------arrivé---------------------------------
-await page.waitForSelector("#arrivee.v37_73")     
-await page.click("#arrivee.v37_73")
-await page.waitForSelector("#depart") //ma9loubin fe site
-await page.$eval('#depart',(e,date_arrivee) => {e.value=date_arrivee},date_arrivee);
-await page.click("#arrivee.v37_73")
+// repository1.getHotels(process.env.urlTunisieBooking)
 
-//------------------------------depart---------------------------------
-
-await page.waitForSelector(".col_dep") 
-await page.click(".col_dep")
-await page.waitForSelector("input#checkout.checkout") 
-await page.$eval('input#checkout.checkout',(e,date_depart) => {e.value=date_depart},date_depart); 
-
-//------------------------------chambre/adultes/enfants---------------------------------
-
-await page.waitForSelector("#chdest") 
-await page.click("#chdest")
-await delay(3000)
-await page.waitForSelector("#adultes1") 
-await page.$eval('#adultes1',(e,nbrAdulte) => e.value=nbrAdulte,nbrAdulte); 
-await page.waitForSelector("#enfants1")
-await page.$eval('#enfants1',(e,nbrEnfant) =>{ e.value=nbrEnfant},nbrEnfant);
+// repository1.getHotels(process.env.urlTunisieBooking)
 
 
-switch (nbrEnfant) {
-    case 0:
-       
-      break;
-    case 1:
-        await page.waitForSelector("#age1_1") 
-        await page.select('#age1_1', `${process.env.ageEnfant1}`)
-      break;
-    case 2:
-        await page.waitForSelector("#age1_1") 
-        await page.select('#age1_1', `${process.env.ageEnfant1}`)
-        await page.waitForSelector("#age1_2") 
-        await page.select('#age1_2', `${process.env.ageEnfant2}`)
-      break;
-    default:
-        await page.waitForSelector("#age1_1") 
-        await page.select('#age1_1', `${process.env.ageEnfant1}`)
-        await page.waitForSelector("#age1_2") 
-        await page.select('#age1_2',`${process.env.ageEnfant2}`)
-        await page.waitForSelector("#age1_3") 
-        await page.select('#age1_3', `${process.env.ageEnfant3}`)
-      
-}
-
-
-//------------------------------search---------------------------------
-await page.waitForSelector(".v194_22") 
-await page.click(".v194_22")
-await delay(2000)
-// -----------------que les hotels disponible--------
-await page.waitForSelector("#btn_filtre_hotel_dispo_bleu") 
-await page.click("#btn_filtre_hotel_dispo_bleu")
-await delay(2000) 
-
-
-
-
-
-
-
-
-
-const  HotelsPromo= await page.evaluate(()=>{
-    
-   
-    
-    let tab_ch= Array.from(document.querySelectorAll(".span_tripadv")).map(x=>{return x.textContent.trim()})
-    console.log("🚀 ~ HotelsPromo ~ tabch1:", tab_ch)
-    let rate=[]
-    
-    let ch=""
-    for(let i=0;i<tab_ch.length-1;i++){
-        if((i%2)==0){ch=tab_ch[i]+"/"+"5"+" "+tab_ch[i+1]
-        rate.push(ch)}
-
-    }
-    
-    let prices=[]
-    let prices1= Array.from(document.querySelectorAll(".prix_tot")).map(x=>{return{ actual_prix:x.textContent.trim()}})
-    for(p of prices1){
-        
-        if(p.actual_prix!=="TND"){
-            prices.push(p)
-    
-        }
-    }
-    
-     
-    let old_prices=[]
-    let old_prices1=Array.from(document.querySelectorAll(".prix_barre")).map(x=>{return{ old_prix:x.textContent.trim()}})
-    for(p of old_prices1){
-        if(p.old_prix!=="TND"){
-            old_prices.push(p)
-        }
-    }
-    
-    let names =Array.from(document.querySelectorAll(".span_lib_hot")).map(x=>{return{ name:x.textContent.trim()}})
-    let HotelsPromo=[]
-    for(let i=0;i<prices.length;i++){
-        HotelsPromo.push({name:names[i].name,olde_price:old_prices[i].old_prix,actual_price:prices[i].actual_prix,rate:rate[i]})
-    }
-    return HotelsPromo
-    
-})
- 
-
-console.log("🚀 ~ getPrice ~ HotelsPromo:", HotelsPromo)
-await page.waitForSelector("#tailleprix") 
-await page.click("#tailleprix")
-
-// browser.close() 
-   
-     
-} 
- 
- 
-getPrice(process.env.urlTunisieBooking,process.env.destination,process.env.date_depart,process.env.date_arrivee,process.env.nbrAdulte,process.env.nbrEnfant)
